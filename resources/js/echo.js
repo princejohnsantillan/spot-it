864efde7f5da3ed9b5a811eabf56533de578c699
Prince John Santillan <mail@princejohnsantillan.net>
2026-01-05 12:45:05 +0800

    wip

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,13 +3,15 @@ import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
+const useTLS = (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https';
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    forceTLS: useTLS,
+    enabledTransports: useTLS ? ['ws', 'wss'] : ['ws'],
     authEndpoint: '/broadcasting/auth',
 });
